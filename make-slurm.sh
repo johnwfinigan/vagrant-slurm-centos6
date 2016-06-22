4eb8beeb8c9aa64d9c6ffb7e78b020303eec51af
Jacob Thompson <jacthomp@mymail.mines.edu>
2016-06-22 14:36:58 -0600

    changed make-slurm to also start the daemons

--- make-slurm.sh
+++ make-slurm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #initial setup
+
 if [ hostname = "head" ]; 
 then	
 	cd /vagrant/RPMS/x86_64/head
@@ -9,9 +10,18 @@ else
 fi
 
 #Install slurm
+
 sudo rpm -i slurm*.rpm
 
 #move config file and slurm setup
 
 ldconfig -n /usr/lib
 sudo cp /vagrant/slurm.conf /etc/slurm/slurm.conf
+
+#start the daemons
+if [ hostname = "head"];
+then
+	sudo /# /etc/rc.d/init.d/slurmctld start
+else
+	sudo /# /etc/rc.d/init.d/slurmd start
+fi
